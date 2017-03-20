c33b9f9240801f211f19e3d2de3d3b169165474b
VanTuan Le <van-tuan.le@grenoble-inp.org>
2017-03-20 21:53:04 +0100

    [ABRO] finir Arbre optimal

--- src/compileBST.c
+++ src/compileBST.c
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <assert.h>
 #include <string.h>
+#include <float.h>
 
 /**
  * Convetir string de longue l en long 
@@ -89,37 +90,85 @@ int main (int argc, char *argv[]) {
  * p[i] probabilité d'accèss à l'élément s[i] i=0..n-1
  */    
  
-    long p[n]; // probabilité d'accèss à l'élément s[i] i = 0..n-1
-    int pCourant = 0;
+    long o[n]; // probabilité d'accèss à l'élément i i = 0..n-1
+    int oCourant = 0;
     char carCourant = fgetc(freqFile); // caractère utilisant pour parcourir la file
     char motCourant[19]; // 19 car LONG_MAX = 9223372036854775807(19 chiffres)
     int longue = 0; // compter la longue du mot courrant
-    while(carCourant != EOF){  // on continue jusqu'à EOF
+    while(carCourant != EOF && oCourant<n){  // on continue jusqu'à EOF
         if(carCourant == '\n' || carCourant == '\t' || carCourant == ' '){
             if(longue != 0) {
-                p[pCourant] = convertirEnLong(motCourant,longue);
-                printf("p[%d] = %d  ",pCourant,p[pCourant]); // on l'affiche
-                pCourant++;
+                o[oCourant] = convertirEnLong(motCourant,longue);
+                printf("o[%d] = %d  ",oCourant,o[oCourant]); // on l'affiche  
+                oCourant++;
                 longue = 0;
             }
             
         } else if(carCourant < 47 || carCourant > 56){
-            fprintf(stderr, "File is invalid\n"); exit(EXIT_FAILURE);
+            fprintf(stderr, "File is invalid\n"); exit(EXIT_FAILURE); // TODO
         } else {
             motCourant[longue] = carCourant;
             longue++;
         }
         carCourant = fgetc(freqFile); // on lit le caractère
     }  
+    printf("\n");
+    // Si le nombre de numéros lu est inférieur à n  
+    if(oCourant != n){
+        fprintf(stderr, "File is invalid\n"); exit(EXIT_FAILURE); exit(EXIT_FAILURE); //TODO
+    }
+    // Caculer p[i]
+    float p[n];
+    int i;
+    unsigned long sommeO = 0;
+    for(i=0; i<n ; i++) sommeO += o[i];
+    for(i=0; i<n ; i++) p[i] = ((float) o[i])/sommeO;
+    for(i=0; i<n ; i++) printf("p[%d] = %5.5f ",i,p[i]);
+    printf("\n");
 
-  printf("\n");
+/**
+ * Arbre Binaire de Recherche Optimal
+ * Les clefs 1,2,...n avec posibilité P1, P2, .... Pn
+ * Tij un ABR pour les clefs ai, a(i+1), ... aj
+ * w[i][j] = P[i] + P[i+1] + .. P[j]
+ * r[i][j] la racine de l'arbre Tij;
+ * e[i][j] le coût de l'arbre Tij
+ */
+    float e[n+2][n+1], w[n+1][n+1];
+    int r[n+1][n+1];
+    int j,l,k,t;
+    for(i=1;i<n+2;i++)
+    for(j=0;j<n+1;j++)
+        e[i][j] = FLT_MAX;
+    e[n+1][n] = 0;
+    for(i=1; i<n+1;i++){
+        e[i][i-1] = 0;
+        e[i][i] = p[i-1];
+        w[i][i] = p[i-1];
+    }
+    for(l=1;l<n;l++)
+    for(i=1;i<n-l+1;i++){
+        j = i+l;
+        w[i][j] = w[i][j-1] + p[j-1];
+        for(k=i;k<j+1;k++){
+            t = e[i][k-1] + e[k+1][j] + w[i][j];
+            if(t<e[i][j]) {
+                e[i][j] = t;
+                r[i-1][j-1] = k-1;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    for(j=i+1;j<n;j++)
+        printf("racine(%d,%d) = %d   ", i, j, r[i][j]);
+
+    printf("\n");
   fclose(freqFile);
-
-
   return 0;
 }
 
 long convertirEnLong(char * string, int l){
     if(l == 0) return 0;
-    else return string[l-1]-47 + 10*convertirEnLong(string, l -1);
+    else return string[l-1]-48 + 10*convertirEnLong(string, l -1);
 }
